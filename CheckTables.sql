be014ae9d82950101a795cf0987df193ebaf6262
Priya <priya123@gmail.com>
2026-06-24 20:12:41 +0530

    Completed Module 3 Exercise 1 Scenario 2

--- CheckTables.sql
+++ CheckTables.sql
@@ -1,39 +1,13 @@
--- -- -- -- SELECT table_name
--- -- -- -- FROM user_tables;
--- -- -- SELECT table_name
--- -- -- FROM user_tables
--- -- -- WHERE table_name IN
--- -- -- (
--- -- -- 'CUSTOMERS',
--- -- -- 'ACCOUNTS',
--- -- -- 'TRANSACTIONS',
--- -- -- 'LOANS',
--- -- -- 'EMPLOYEES'
--- -- -- );
--- -- SELECT
--- -- CustomerID,
--- -- Name,
--- -- TRUNC(
--- -- MONTHS_BETWEEN(
--- -- SYSDATE,
--- -- DOB
--- -- )/12
--- -- ) AS AGE
--- -- FROM Customers;
--- -- SELECT *
--- -- FROM Loans;
+--ControlStructures.sql
 -- SELECT
+-- LoanID,
 -- CustomerID,
--- Name,
--- TRUNC(
--- MONTHS_BETWEEN(
--- SYSDATE,
--- DOB
--- )/12
--- ) AS AGE
--- FROM Customers;
+-- InterestRate
+-- FROM Loans;
+
 SELECT
-LoanID,
 CustomerID,
-InterestRate
-FROM Loans;
+Name,
+Balance,
+IsVIP
+FROM Customers;
