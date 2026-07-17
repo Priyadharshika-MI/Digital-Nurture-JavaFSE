067ecbaa5d43845cfb907b94a24c30dcb8db4c42
Priya <priya123@gmail.com>
2026-07-17 20:25:52 +0530

    Completed Module 7 Exercise 2 - Load SimpleDateFormat from Spring XML

---
+++ Module-6-Spring-Data-JPA-Hibernate/Exercise-2-DifferenceBetweenJPAHibernateAndSpringDataJPA/spring-data-jpa-handson/src/main/java/com/cognizant/ormlearn/SpringDataJpaHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.ormlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaHandsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaHandsonApplication.class, args);
+	}
+
+}
