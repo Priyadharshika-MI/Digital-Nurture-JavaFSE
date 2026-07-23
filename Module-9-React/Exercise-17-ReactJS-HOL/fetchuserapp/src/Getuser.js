dff58a8e2b1b7ca30ff3581d73d7801a716c9095
Priya <priya123@gmail.com>
2026-07-23 21:34:57 +0530

    Completed Module 9 Exercise 17 - Fetch REST API

---
+++ Module-9-React/Exercise-17-ReactJS-HOL/fetchuserapp/src/Getuser.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class Getuser extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      person: {},
+      loading: true
+    };
+  }
+
+  async componentDidMount() {
+
+    const url = "https://api.randomuser.me/";
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    this.setState({
+      person: data.results[0],
+      loading: false
+    });
+
+    console.log(data.results[0]);
+  }
+
+  render() {
+
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "50px"
+        }}
+      >
+
+        <h1>
+          {this.state.person.name.title}{" "}
+          {this.state.person.name.first}
+        </h1>
+
+        <img
+          src={this.state.person.picture.large}
+          alt="User"
+        />
+
+      </div>
+    );
+  }
+}
+
+export default Getuser;
